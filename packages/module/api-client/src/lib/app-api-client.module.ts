d591419d67ddc669d9f2c2cfa22b615fceecf577
michaljarnot <m.jarnot@yahoo.com>
2022-02-06 19:44:04 +0100

    fix: strictly type for root module declaration

--- packages/module/api-client/src/lib/app-api-client.module.ts
+++ packages/module/api-client/src/lib/app-api-client.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http'
-import { NgModule } from '@angular/core'
+import { ModuleWithProviders, NgModule } from '@angular/core'
 import { IConfiguration } from './api.module.types'
 import { API_URL } from './services/api.service.tokens'
 
@@ -7,7 +7,9 @@ import { API_URL } from './services/api.service.tokens'
   imports: [HttpClientModule],
 })
 export class AppApiClientModule {
-  public static forRoot(config: IConfiguration) {
+  public static forRoot(
+    config: IConfiguration
+  ): ModuleWithProviders<AppApiClientModule> {
     return {
       ngModule: AppApiClientModule,
       providers: [{ provide: API_URL, useValue: config.apiUrl }],
